24196aabd7dfb8bd84aa22a9d81db3ac1a7ee604
Vubaphuc <112857123+Vubaphuc@users.noreply.github.com>
2023-04-08 18:43:58 +0700

    update
    update

--- demo-security/src/main/java/com/example/demosecurity/security/SecurityConfig.java
+++ demo-security/src/main/java/com/example/demosecurity/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,6 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(
+        jsr250Enabled = true,
+        securedEnabled = true
+)
 public class SecurityConfig {
 
     @Bean
