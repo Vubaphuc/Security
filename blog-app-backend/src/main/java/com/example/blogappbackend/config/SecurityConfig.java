03491787afc74f05a5b0b90292cb973bb04336fa
Vubaphuc <112857123+Vubaphuc@users.noreply.github.com>
2023-04-16 22:08:25 +0700

    update
    update

--- blog-app-backend/src/main/java/com/example/blogappbackend/config/SecurityConfig.java
+++ blog-app-backend/src/main/java/com/example/blogappbackend/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
         String [] PUBLIC = {
-                "/api/v1/public/**"
+                "/api/v1/public/**",
+                "/"
         };
         http
                 .csrf().disable()
